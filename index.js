4def93dc3ce18de886efe8fd2e23d19e5d179bb4
Mayank Sinhal <sinhal.mayank.mk@gmail.com>
2023-08-20 14:53:52 +0530

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 connectToMongo();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "35mb" }));
